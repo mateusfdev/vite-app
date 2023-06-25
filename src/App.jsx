d2253dd4b6783f7b3a34e322635d512dc4686314
WEEDZERA <MATEUSDEVV@GMAIL.COM>
2023-06-24 23:36:15 -0300

    second

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,14 @@
 import { useState } from 'react'
-
-
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+  // eslint-disable-next-line no-empty-pattern
+  const [] = useState(0)
 
   return (
-    <div className="app"><h1>Front Beginers</h1></div>
+    <div className="app">
+      <h1>Front Beginers</h1>
+    </div>
   )
 }
 
